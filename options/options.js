a641539c279d5fd064d503bb1cbedc4367172b82
Crowdbotics <team@crowdbotics.com>
2023-01-27 17:37:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "destiny-meet-38530",
+  url: "https://destiny-meet-38530.botics.co",
+  api: "https://destiny-meet-38530.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
